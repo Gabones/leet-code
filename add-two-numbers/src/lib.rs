87dc75003055a6f85982f51009ef8e60dcc00c71
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-10-09 00:37:53 -0300

    optimized solution

--- add-two-numbers/src/lib.rs
+++ add-two-numbers/src/lib.rs
@@ -5,25 +5,29 @@ use listnode::{to_list, to_vector, ListNode};
 #[cfg(test)]
 mod tests;
 
-pub fn add_two_numbers(
-    l1: Option<Box<ListNode>>,
-    l2: Option<Box<ListNode>>,
-) -> Option<Box<ListNode>> {
-    let v1 = to_vector(&l1);
-    let v2 = to_vector(&l2);
+pub struct Solution;
 
-    let mut carry = 0;
-    let mut vec_result: Vec<i32> = vec![];
-    let mut i = 0;
-    vec_result = loop {
-        carry += v1.get(i).unwrap_or(&0) + v2.get(i).unwrap_or(&0);
-        vec_result.push(carry % 10);
-        carry = carry / 10;
-        i += 1;
-        if v1.get(i).is_none() && v2.get(i).is_none() && carry == 0 {
-            break vec_result;
-        }
-    };
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let v1 = to_vector(&l1);
+        let v2 = to_vector(&l2);
 
-    return to_list(vec_result);
+        let mut carry = 0;
+        let mut vec_result: Vec<i32> = vec![];
+        let mut i = 0;
+        vec_result = loop {
+            carry += v1.get(i).unwrap_or(&0) + v2.get(i).unwrap_or(&0);
+            vec_result.push(carry % 10);
+            carry = carry / 10;
+            i += 1;
+            if v1.get(i).is_none() && v2.get(i).is_none() && carry == 0 {
+                break vec_result;
+            }
+        };
+
+        return to_list(vec_result);
+    }
 }
--- add-two-numbers/src/tests.rs
+++ add-two-numbers/src/tests.rs
@@ -1,5 +1,5 @@
-use crate::add_two_numbers;
 use crate::listnode::to_list;
+use crate::Solution;
 
 #[test]
 //#[ignore]
@@ -7,7 +7,7 @@ fn case_1() {
     let l1 = to_list(vec![2, 4, 3]);
     let l2 = to_list(vec![5, 6, 4]);
 
-    assert_eq!(add_two_numbers(l1, l2), to_list(vec![7, 0, 8]));
+    assert_eq!(Solution::add_two_numbers(l1, l2), to_list(vec![7, 0, 8]));
 }
 
 #[test]
@@ -15,7 +15,7 @@ fn case_1() {
 fn case_2() {
     let l1 = to_list(vec![0]);
     let l2 = to_list(vec![0]);
-    assert_eq!(add_two_numbers(l1, l2), to_list(vec![0]));
+    assert_eq!(Solution::add_two_numbers(l1, l2), to_list(vec![0]));
 }
 
 #[test]
@@ -24,7 +24,7 @@ fn case_3() {
     let l1 = to_list(vec![9, 9, 9, 9, 9, 9, 9]);
     let l2 = to_list(vec![9, 9, 9, 9]);
     assert_eq!(
-        add_two_numbers(l1, l2),
+        Solution::add_two_numbers(l1, l2),
         to_list(vec![8, 9, 9, 9, 0, 0, 0, 1])
     );
 }
@@ -43,7 +43,7 @@ fn case_1566() {
         9,
     ]);
     assert_eq!(
-        add_two_numbers(l1, l2),
+        Solution::add_two_numbers(l1, l2),
         to_list(vec![
             7, 0, 8, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8,
             6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 4, 8, 6, 1,
