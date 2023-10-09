e95f2acf89ae919d58da3cdd138ef1cad68ff9f9
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-10-08 22:05:01 -0300

    first commit

---
+++ add-two-numbers/target/debug/deps/add_two_numbers-10a267596eac5d25.d
@@ -0,0 +1,5 @@
+C:\Users\gabao\Exercism\rust\leet-code\add-two-numbers\target\debug\deps\add_two_numbers-10a267596eac5d25.rmeta: src\lib.rs
+
+C:\Users\gabao\Exercism\rust\leet-code\add-two-numbers\target\debug\deps\add_two_numbers-10a267596eac5d25.d: src\lib.rs
+
+src\lib.rs:
---
+++ add-two-numbers/target/debug/deps/add_two_numbers-a7065085dc3f484a.d
@@ -0,0 +1,5 @@
+C:\Users\gabao\Exercism\rust\leet-code\add-two-numbers\target\debug\deps\add_two_numbers-a7065085dc3f484a.rmeta: src\lib.rs
+
+C:\Users\gabao\Exercism\rust\leet-code\add-two-numbers\target\debug\deps\add_two_numbers-a7065085dc3f484a.d: src\lib.rs
+
+src\lib.rs:
