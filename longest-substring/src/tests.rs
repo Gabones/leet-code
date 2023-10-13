626786be188f52e5434101675a622b1347cc10cf
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-10-12 21:08:34 -0300

    longest-substring

---
+++ longest-substring/src/tests.rs
@@ -0,0 +1,62 @@
+use crate::Solution;
+
+#[test]
+// #[ignore]
+fn case_1() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("abcabcbb")),
+        3
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_2() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("bbbbb")),
+        1
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_3() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("pwwkew")),
+        3
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_340() {
+    assert_eq!(Solution::length_of_longest_substring(String::from(" ")), 1);
+}
+
+#[test]
+// #[ignore]
+fn case_407() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("dvdf")),
+        3
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_477() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("anviaj")),
+        5
+    );
+}
+
+// TODO tenho que voltar no índice onde ocorreu o erro e não uma casa decimal
+#[test]
+// #[ignore]
+fn case_702() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("busvutpwmu")),
+        7
+    );
+}
