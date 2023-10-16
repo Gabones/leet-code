aaf4b464e3da6525185883ee28cecca1b7937219
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-10-15 22:44:28 -0300

    fix: pub impl

--- median-sorted-arrays/src/lib.rs
+++ median-sorted-arrays/src/lib.rs
@@ -3,7 +3,7 @@ mod tests;
 
 pub struct Solution;
 
-pub impl Solution {
+impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let mut p1 = 0;
         let mut p2 = 0;
