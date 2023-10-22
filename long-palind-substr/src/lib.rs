c06df01226d8fbcfe18f57673e41ece0e64635a3
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-10-22 01:07:20 -0300

    longest palindromic substring

--- long-palind-substr/src/lib.rs
+++ long-palind-substr/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Solution;
+
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {
+        let n = s.len();
+        let mut dp = vec![vec![false; n]; n];
+        let mut ans = [0,0];
+
+        for i in 0..n {
+            dp[i][i] = true;
+        }
+
+        for i in 0..(n-1) {
+            if s.as_bytes()[i] == s.as_bytes()[i+1] {
+                dp[i][i+1] = true;
+                ans = [i, i+1]
+            }
+        }
+
+        for diff in 2..n {
+            for i in 0..(n - diff) {
+                let j = i + diff;
+                if s.as_bytes()[i] == s.as_bytes()[j] && dp[i+1][j-1] {
+                    dp[i][j] = true;
+                    ans = [i,j]
+                }
+            }
+        }
+
+
+        s[ans[0]..(ans[1]+1)].to_string()
     }
 }
---
+++ long-palind-substr/src/tests.rs
@@ -0,0 +1,28 @@
+use crate::Solution;
+
+#[test]
+// #[ignore]
+fn case_1() {
+    assert!(
+        Solution::longest_palindrome(("babad").to_string()) == "bab" ||
+        Solution::longest_palindrome(("babad").to_string()) == "aba"
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_2() {
+    assert_eq!(
+        Solution::longest_palindrome(("cbbd").to_string()),
+        "bb"
+    );
+}
+
+#[test]
+// #[ignore]
+fn case_3() {
+    assert_eq!(
+        Solution::longest_palindrome(("mwwfjysbkebpdjyabcfkgprtxpwvhglddhmvaprcvrnuxifcrjpdgnktvmggmguiiquibmtviwjsqwtchkqgxqwljouunurcdtoeygdqmijdympcamawnlzsxucbpqtuwkjfqnzvvvigifyvymfhtppqamlgjozvebygkxawcbwtouaankxsjrteeijpuzbsfsjwxejtfrancoekxgfyangvzjkdskhssdjvkvdskjtiybqgsmpxmghvvicmjxqtxdowkjhmlnfcpbtwvtmjhnzntxyfxyinmqzivxkwigkondghzmbioelmepgfttczskvqfejfiibxjcuyevvpawybcvvxtxycrfbcnpvkzryrqujqaqhoagdmofgdcbhvlwgwmsmhomknbanvntspvvhvccedzzngdywuccxrnzbtchisdwsrfdqpcwknwqvalczznilujdrlevncdsyuhnpmheukottewtkuzhookcsvctsqwwdvfjxifpfsqxpmpwospndozcdbfhselfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidwcfcuxinchretgvfaepmgilbrtxgqoddzyjmmupkjqcypdpfhpkhitfegickfszermqhkwmffdizeoprmnlzbjcwfnqyvmhtdekmfhqwaftlyydirjnojbrieutjhymfpflsfemkqsoewbojwluqdckmzixwxufrdpqnwvwpbavosnvjqxqbosctttxvsbmqpnolfmapywtpfaotzmyjwnd").to_string()),
+        "khvhk"
+    );   
+}
