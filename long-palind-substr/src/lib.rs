b1f50f1b5c1374550feb30cb8bb62def287744e1
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-11-09 22:27:51 -0300

    check all long palindrome string

--- long-palind-substr/src/lib.rs
+++ long-palind-substr/src/lib.rs
@@ -4,33 +4,55 @@ mod tests;
 pub struct Solution;
 
 impl Solution {
-    pub fn longest_palindrome(s: String) -> String {
+    pub fn longest_palindrome_check_all(s: String) -> String {
+        pub fn check(s: &String, i: usize, j: usize) -> bool {
+            let [mut left, mut right] = [i, j - 1];
+            while left < right {
+                if s.as_bytes()[left] != s.as_bytes()[right] {
+                    return false;
+                }
+                left += 1;
+                right -= 1;
+            }
+            return true;
+        }
+
+        for length in (0..s.len()).rev() {
+            for start in 0..s.len() - length + 1 {
+                if check(&s, start, start + length) {
+                    return s[start..start + length].to_string();
+                }
+            }
+        }
+        return String::from("");
+    }
+
+    pub fn longest_palindrome_dynamic(s: String) -> String {
         let n = s.len();
         let mut dp = vec![vec![false; n]; n];
-        let mut ans = [0,0];
+        let mut ans = [0, 0];
 
         for i in 0..n {
             dp[i][i] = true;
         }
 
-        for i in 0..(n-1) {
-            if s.as_bytes()[i] == s.as_bytes()[i+1] {
-                dp[i][i+1] = true;
-                ans = [i, i+1]
+        for i in 0..(n - 1) {
+            if s.as_bytes()[i] == s.as_bytes()[i + 1] {
+                dp[i][i + 1] = true;
+                ans = [i, i + 1]
             }
         }
 
         for diff in 2..n {
             for i in 0..(n - diff) {
                 let j = i + diff;
-                if s.as_bytes()[i] == s.as_bytes()[j] && dp[i+1][j-1] {
+                if s.as_bytes()[i] == s.as_bytes()[j] && dp[i + 1][j - 1] {
                     dp[i][j] = true;
-                    ans = [i,j]
+                    ans = [i, j]
                 }
             }
         }
 
-
-        s[ans[0]..(ans[1]+1)].to_string()
+        s[ans[0]..(ans[1] + 1)].to_string()
     }
 }
--- long-palind-substr/src/tests.rs
+++ long-palind-substr/src/tests.rs
@@ -4,25 +4,50 @@ use crate::Solution;
 // #[ignore]
 fn case_1() {
     assert!(
-        Solution::longest_palindrome(("babad").to_string()) == "bab" ||
-        Solution::longest_palindrome(("babad").to_string()) == "aba"
+        Solution::longest_palindrome_check_all(("babad").to_string()) == "bab"
+            || Solution::longest_palindrome_check_all(("babad").to_string()) == "aba"
     );
+    // assert!(
+    //     Solution::longest_palindrome_dynamic(("babad").to_string()) == "bab"
+    //         || Solution::longest_palindrome_dynamic(("babad").to_string()) == "aba"
+    // );
 }
 
 #[test]
 // #[ignore]
 fn case_2() {
     assert_eq!(
-        Solution::longest_palindrome(("cbbd").to_string()),
+        Solution::longest_palindrome_check_all(("cbbd").to_string()),
         "bb"
     );
+    // assert_eq!(
+    //     Solution::longest_palindrome_dynamic(("cbbd").to_string()),
+    //     "bb"
+    // );
 }
 
 #[test]
 // #[ignore]
 fn case_3() {
     assert_eq!(
-        Solution::longest_palindrome(("mwwfjysbkebpdjyabcfkgprtxpwvhglddhmvaprcvrnuxifcrjpdgnktvmggmguiiquibmtviwjsqwtchkqgxqwljouunurcdtoeygdqmijdympcamawnlzsxucbpqtuwkjfqnzvvvigifyvymfhtppqamlgjozvebygkxawcbwtouaankxsjrteeijpuzbsfsjwxejtfrancoekxgfyangvzjkdskhssdjvkvdskjtiybqgsmpxmghvvicmjxqtxdowkjhmlnfcpbtwvtmjhnzntxyfxyinmqzivxkwigkondghzmbioelmepgfttczskvqfejfiibxjcuyevvpawybcvvxtxycrfbcnpvkzryrqujqaqhoagdmofgdcbhvlwgwmsmhomknbanvntspvvhvccedzzngdywuccxrnzbtchisdwsrfdqpcwknwqvalczznilujdrlevncdsyuhnpmheukottewtkuzhookcsvctsqwwdvfjxifpfsqxpmpwospndozcdbfhselfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidwcfcuxinchretgvfaepmgilbrtxgqoddzyjmmupkjqcypdpfhpkhitfegickfszermqhkwmffdizeoprmnlzbjcwfnqyvmhtdekmfhqwaftlyydirjnojbrieutjhymfpflsfemkqsoewbojwluqdckmzixwxufrdpqnwvwpbavosnvjqxqbosctttxvsbmqpnolfmapywtpfaotzmyjwnd").to_string()),
+        Solution::longest_palindrome_check_all(("mwwfjysbkebpdjyabcfkgprtxpwvhglddhmvaprcvrnuxifcrjpdgnktvmggmguiiquibmtviwjsqwtchkqgxqwljouunurcdtoeygdqmijdympcamawnlzsxucbpqtuwkjfqnzvvvigifyvymfhtppqamlgjozvebygkxawcbwtouaankxsjrteeijpuzbsfsjwxejtfrancoekxgfyangvzjkdskhssdjvkvdskjtiybqgsmpxmghvvicmjxqtxdowkjhmlnfcpbtwvtmjhnzntxyfxyinmqzivxkwigkondghzmbioelmepgfttczskvqfejfiibxjcuyevvpawybcvvxtxycrfbcnpvkzryrqujqaqhoagdmofgdcbhvlwgwmsmhomknbanvntspvvhvccedzzngdywuccxrnzbtchisdwsrfdqpcwknwqvalczznilujdrlevncdsyuhnpmheukottewtkuzhookcsvctsqwwdvfjxifpfsqxpmpwospndozcdbfhselfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidwcfcuxinchretgvfaepmgilbrtxgqoddzyjmmupkjqcypdpfhpkhitfegickfszermqhkwmffdizeoprmnlzbjcwfnqyvmhtdekmfhqwaftlyydirjnojbrieutjhymfpflsfemkqsoewbojwluqdckmzixwxufrdpqnwvwpbavosnvjqxqbosctttxvsbmqpnolfmapywtpfaotzmyjwnd").to_string()),
         "khvhk"
-    );   
+    );
+    // assert_eq!(
+    //     Solution::longest_palindrome_dynamic(("mwwfjysbkebpdjyabcfkgprtxpwvhglddhmvaprcvrnuxifcrjpdgnktvmggmguiiquibmtviwjsqwtchkqgxqwljouunurcdtoeygdqmijdympcamawnlzsxucbpqtuwkjfqnzvvvigifyvymfhtppqamlgjozvebygkxawcbwtouaankxsjrteeijpuzbsfsjwxejtfrancoekxgfyangvzjkdskhssdjvkvdskjtiybqgsmpxmghvvicmjxqtxdowkjhmlnfcpbtwvtmjhnzntxyfxyinmqzivxkwigkondghzmbioelmepgfttczskvqfejfiibxjcuyevvpawybcvvxtxycrfbcnpvkzryrqujqaqhoagdmofgdcbhvlwgwmsmhomknbanvntspvvhvccedzzngdywuccxrnzbtchisdwsrfdqpcwknwqvalczznilujdrlevncdsyuhnpmheukottewtkuzhookcsvctsqwwdvfjxifpfsqxpmpwospndozcdbfhselfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidwcfcuxinchretgvfaepmgilbrtxgqoddzyjmmupkjqcypdpfhpkhitfegickfszermqhkwmffdizeoprmnlzbjcwfnqyvmhtdekmfhqwaftlyydirjnojbrieutjhymfpflsfemkqsoewbojwluqdckmzixwxufrdpqnwvwpbavosnvjqxqbosctttxvsbmqpnolfmapywtpfaotzmyjwnd").to_string()),
+    //     "khvhk"
+    // );
+}
+
+#[test]
+// #[ignore]
+fn case_4() {
+    assert_eq!(
+        Solution::longest_palindrome_check_all(("a").to_string()),
+        "a"
+    );
+    // assert_eq!(
+    //     Solution::longest_palindrome_dynamic(("a").to_string()),
+    //     "khvhk"
+    // );
 }
