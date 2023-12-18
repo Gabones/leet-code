8f80d2771842e8988963f80de7a4a2016c977f38
Gabones <gabaoalb@gmail.com>
2023-12-17 23:57:41 -0300

    zigzag optimizations

--- zigzag-conversion/src/lib.rs
+++ zigzag-conversion/src/lib.rs
@@ -10,6 +10,34 @@ impl Solution {
         }
 
         let num_rows = num_rows as usize;
+        let mut vector = vec![String::from(""); num_rows];
+        let mut row = 0;
+        let mut reverse = false;
+
+        for c in s.chars() {
+            vector[row] += &c.to_string();
+
+            if reverse {
+                row -= 1;
+            } else {
+                row += 1;
+            }
+
+            if row == 0 || row == (num_rows - 1) {
+                reverse = !reverse;
+            }
+        }
+
+        vector.join("")
+    }
+
+
+    pub fn convert_pretty_but_inefficient(s: String, num_rows: i32) -> String {
+        if num_rows == 1 {
+            return s;
+        }
+
+        let num_rows = num_rows as usize;
         let mut matrix: Vec<Vec<char>> = vec![vec!['#'; s.len()]; num_rows];
         let [mut row, mut column] = [0, 0];
         let mut reverse = false;
