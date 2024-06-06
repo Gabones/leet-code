38e7eae5bb1d89b598eff1fc405323f6469e4ff4
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2024-06-06 01:03:38 -0300

    most water brute force

--- container-with-most-water/src/lib.rs
+++ container-with-most-water/src/lib.rs
@@ -9,9 +9,8 @@ impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let mut area_max = 0;
         for i in 0..height.len() {
-            for j in 1..height.len() {
-                let min = cmp::min(height[i], height[j]);
-                let curr_area = min * (j as i32 - i as i32);
+            for j in (i + 1)..height.len() {
+                let curr_area = cmp::min(height[i], height[j]) * (j as i32 - i as i32);
                 area_max = cmp::max(curr_area, area_max);
             }
         }
