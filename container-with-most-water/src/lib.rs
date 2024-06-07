0605f7ea3b19c0046bcfc1bf31eda379537bf7ac
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2024-06-06 22:36:44 -0300

    pointer container most water

--- container-with-most-water/src/lib.rs
+++ container-with-most-water/src/lib.rs
@@ -7,6 +7,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
+        let (mut area_max, mut left, mut right) = (0, height.len() - 1, 0);
+
+        while left < right {
+            let min = cmp::min(height[left], height[right]);
+            let curr_area = min * (right - left) as i32;
+            area_max = cmp::max(curr_area, area_max);
+
+            if min == height[left] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        area_max
+    }
+
+    pub fn max_area_brute_force(height: Vec<i32>) -> i32 {
         let mut area_max = 0;
         for i in 0..height.len() {
             for j in (i + 1)..height.len() {
