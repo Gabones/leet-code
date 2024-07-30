278ed920c4215b38b0022e66cb64d27169242015
Gabriel A da Rosa <gabriel.rosa@effectiveerp.com.br>
2024-07-30 13:36:10 -0300

    forbiden index not handled yet

--- three-sum/src/lib.rs
+++ three-sum/src/lib.rs
@@ -5,11 +5,35 @@ pub struct Solution;
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let pointer_1 = 0;
-        let pointer_2 = nums.len();
+        let mut p_1 = 0;
+        let mut p_2 = nums.len() - 1;
+        let mut result: Vec<Vec<i32>> = Vec::new();
+        let mut last_up = true;
 
-        while pointer_1 < pointer_2 {}
+        while p_1 < p_2 {
+            for (idx, _) in nums.iter().enumerate() {
+                if idx != p_1 && idx != p_2 {
+                    println!(
+                        "p_1: {:?} idx: {:?} p_2: {:?}",
+                        nums[p_1], nums[idx], nums[p_2]
+                    );
+                    if nums[p_1] + nums[p_2] + nums[idx] == 0 {
+                        let mut new_vec = vec![nums[p_1], nums[p_2], nums[idx]];
+                        new_vec.sort();
+                        result.push(new_vec);
+                    }
+                }
+            }
 
-        return vec![vec![0]];
+            if last_up {
+                p_1 += 1;
+                last_up = false;
+            } else {
+                p_2 -= 1;
+                last_up = true;
+            }
+        }
+
+        return result;
     }
 }
--- three-sum/src/tests.rs
+++ three-sum/src/tests.rs
@@ -7,3 +7,15 @@ fn case_1() {
         vec![[-1, -1, 2], [-1, 0, 1]]
     )
 }
+
+#[test]
+fn case_2() {
+    let v: Vec<Vec<i32>> = Vec::new();
+    assert_eq!(Solution::three_sum(vec![0, 1, 1]), v)
+}
+
+#[test]
+fn case_3() {
+    let v: Vec<Vec<i32>> = Vec::new();
+    assert_eq!(Solution::three_sum(vec![0, 0, 0]), v)
+}
