685194aa28d8f8c15ed55dfaadb68d584b495598
Gabones <gabaoalb@gmail.com>
2024-11-18 01:56:25 +0000

    roman to integer and longest common prefix files

---
+++ tests/roman_to_integer_test.rs
@@ -0,0 +1,19 @@
+use leet_code::roman_to_integer::Solution;
+
+#[test]
+// #[ignore]
+fn case_1() {
+    assert_eq!(Solution::roman_to_int("III".to_string()), 3);
+}
+
+#[test]
+// #[ignore]
+fn case_2() {
+    assert_eq!(Solution::roman_to_int("LVIII".to_string()), 58);
+}
+
+#[test]
+// #[ignore]
+fn case_3() {
+    assert_eq!(Solution::roman_to_int("MCMXCIV".to_string()), 1994);
+}
