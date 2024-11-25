65332a2bafa2917896f987ad0506f0284dcf739c
Gabones <gabaoalb@gmail.com>
2024-11-25 23:31:23 +0000

    added new files for top k elements

---
+++ tests/top_k_frequent_elements_test.rs
@@ -0,0 +1,18 @@
+use leet_code::top_k_frequent_elements::Solution;
+
+#[test]
+fn case_1() {
+    assert_eq!(
+        Solution::top_k_frequent(vec![1,1,1,2,2,3], 2),
+        vec![1,2]
+    )
+}
+
+#[test]
+fn case_2() {
+    assert_eq!(
+        Solution::top_k_frequent(vec![1], 1),
+        vec![1]
+    )
+}
+
