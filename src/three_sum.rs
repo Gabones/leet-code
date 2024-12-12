908aa3711432462b019c330b1e51cec31c5153f2
Gabones <gabaoalb@gmail.com>
2024-12-12 16:34:44 +0000

    three sum others approachs

--- src/three_sum.rs
+++ src/three_sum.rs
@@ -1,6 +1,64 @@
+use std::collections::{HashMap, HashSet};
+
 pub struct Solution;
 
 impl Solution {
+    // Approach 1 - Brute Force
+    pub fn three_sum_brute_force(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort();
+        let mut set_result = HashSet::new();
+
+        for i in 0..nums.len() {
+            for j in i+1..nums.len() {
+                for k in j+1..nums.len() {
+                    if nums[i] + nums[j] + nums[k] == 0 {
+                        set_result.insert(vec![nums[i], nums[j], nums[k]]);
+                    }
+                }
+            }
+        }
+        
+        let mut result:Vec<Vec<i32>> = set_result.into_iter().collect();
+        result.sort();
+        result
+    }
+
+    // Approach 2 - HashMap
+    pub fn three_sum_hashmap(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort();
+        let mut count: HashMap<i32, i32> = HashMap::new();
+
+        for num in nums.iter() {
+           *count.entry(*num).or_insert(0) += 1;
+        }
+
+        let mut result: Vec<Vec<i32>> = Vec::new();
+        for i in 0..nums.len() {
+            *count.entry(i as i32).or_insert(0) -= 1;
+            if i > 0 && nums[i] == nums[i-1] {
+                continue;
+            }
+
+            for j in i+1..nums.len() {
+                *count.entry(i as i32).or_insert(0) -= 1;
+                if j > i + 1 && nums[j] == nums[j-1] {
+                    continue;
+                }
+                
+                let target = -(nums[i] + nums[j]);
+                if count.entry(target) > 0 {
+                    result.push(vec![nums[i], nums[j], target]);
+                }
+            }
+
+            for j in i+1..nums.len() {
+                *count.entry(nums[j]).or_insert(0) += 1;
+            }
+        }
+        result
+    }
+
+    // Approach 3 - Two Pointers
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort();
         let mut result: Vec<Vec<i32>> = Vec::new();
--- tests/three_sum_test.rs
+++ tests/three_sum_test.rs
@@ -3,7 +3,7 @@ use leet_code::three_sum::Solution;
 #[test]
 fn case_1() {
     assert_eq!(
-        Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
+        Solution::three_sum_hashmap(vec![-1, 0, 1, 2, -1, -4]),
         vec![[-1, -1, 2], [-1, 0, 1]]
     )
 }
