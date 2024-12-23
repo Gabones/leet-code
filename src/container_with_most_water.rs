368ae855c8a73b0ad69f967cece04532363c5b09
Gabones <gabaoalb@gmail.com>
2024-12-23 18:53:45 +0000

    comments on container with most water

--- src/container_with_most_water.rs
+++ src/container_with_most_water.rs
@@ -3,6 +3,21 @@ use std::cmp;
 pub struct Solution;
 
 impl Solution {
+    // Approach 1 - Brute Force  
+    pub fn max_area_brute_force(height: Vec<i32>) -> i32 {
+        let mut area_max = 0;
+
+        for i in 0..height.len() {
+            for j in (i + 1)..height.len() {
+                let curr_area = cmp::min(height[i], height[j]) * (j as i32 - i as i32);
+                area_max = cmp::max(curr_area, area_max);
+            }
+        }
+
+        area_max
+    }
+    
+    // Approach 2 - Two Pointers
     pub fn max_area(height: Vec<i32>) -> i32 {
         let (mut area_max, mut left, mut right) = (0, 0, height.len() - 1);
 
@@ -17,20 +32,6 @@ impl Solution {
                 right -= 1;
             }
         }
-
-        area_max
-    }
-
-    pub fn max_area_brute_force(height: Vec<i32>) -> i32 {
-        let mut area_max = 0;
-        for i in 0..height.len() {
-            for j in (i + 1)..height.len() {
-                let curr_area = cmp::min(height[i], height[j]) * (j as i32 - i as i32);
-                area_max = cmp::max(curr_area, area_max);
-            }
-        }
-
-        println!("{area_max}");
 
         area_max
     }
