0dfc49b6444c9df8e681a418a21309dffea69c29
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2025-01-11 23:58:02 -0300

    daily temperatures complement

--- src/daily_temperatures.rs
+++ src/daily_temperatures.rs
@@ -1,7 +1,24 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+    pub fn daily_temperatures_brute_force(temperatures: Vec<i32>) -> Vec<i32> {
+        let n = temperatures.len();
+        let mut result: Vec<i32> = vec![0; n];
+
+        for i in 0..result.len() {
+            for j in i..result.len() {
+                if temperatures[j] > temperatures[i] {
+                    result[i] = (j - i) as i32;
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+
+    // Approach 2.1 - Reverse Monotonic Stack
+    pub fn daily_temperatures_reverse_stack(temperatures: Vec<i32>) -> Vec<i32> {
         let mut stack: Vec<(i32, usize)> = Vec::new();
         let mut result: Vec<i32> = Vec::new();
 
@@ -16,11 +33,28 @@ impl Solution {
                 while stack.len() > 0 && temperatures[index] >= stack.last().unwrap().0 {
                     stack.pop();
                 }
-                result.push((stack.last().unwrap_or(&(temperatures[index], index)).1 - index) as i32);
+                result
+                    .push((stack.last().unwrap_or(&(temperatures[index], index)).1 - index) as i32);
                 stack.push((temperatures[index], index));
             }
         }
 
         result.into_iter().rev().collect()
+    }
+
+    // Approach 2.2 - Monotonic Stack
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut stack: Vec<(i32, usize)> = Vec::new();
+        let mut result: Vec<i32> = vec![0; temperatures.len()];
+
+        for (index, &temp) in temperatures.iter().enumerate() {
+            while stack.len() > 0 && temp > stack.last().unwrap().0 {
+                let (_s_temp, s_index) = stack.pop().unwrap();
+                result[s_index] = (index - s_index) as i32;
+            }
+            stack.push((temp, index));
+        }
+
+        result
     }
 }
--- tests/daily_temperatures_test.rs
+++ tests/daily_temperatures_test.rs
@@ -11,24 +11,23 @@ fn case_1() {
 #[test]
 fn case_2() {
     assert_eq!(
-        Solution::daily_temperatures(vec![30,40,50,60]),
-        vec![1,1,1,0]
+        Solution::daily_temperatures(vec![30, 40, 50, 60]),
+        vec![1, 1, 1, 0]
     )
 }
 
 #[test]
 fn case_3() {
     assert_eq!(
-        Solution::daily_temperatures(vec![30,60,90]),
-        vec![1,1,0]
+        Solution::daily_temperatures(vec![30, 60, 90]),
+        vec![1, 1, 0]
     )
 }
 
 #[test]
 fn case_4() {
     assert_eq!(
-        Solution::daily_temperatures(vec![89,62,70,58,47,47,46,76,100,70]),
-        vec![8,1,5,4,3,2,1,1,0,0]
+        Solution::daily_temperatures(vec![89, 62, 70, 58, 47, 47, 46, 76, 100, 70]),
+        vec![8, 1, 5, 4, 3, 2, 1, 1, 0, 0]
     )
 }
-
